da35751cdc05bd4520546cda6fe2eecfadbbf8c3
NieuwendijkB <93547503+NieuwendijkB@users.noreply.github.com>
2021-12-08 21:12:23 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+const makePizza = function () {
+  console.log("make the dough");
+  console.log("spread topping on the dough");
+  console.log("bake in oven");
+};
+//by running just this i get no code, because the function is not being called.
+
+const makeSushi = function () {
+  console.log("catch fish");
+  console.log("cut fish");
+  console.log("roll fish in rice");
+};
+
+makeSushi();
+makeSushi();
+makeSushi();
+makePizza();
+makePizza();
